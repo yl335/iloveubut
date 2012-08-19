a4e29e0f9b3e78745f2f4cbdad4e6da4d9fd7767
Sara Lin <slin@splunk.com>
2012-08-19 09:48:30 -0700

    Added postgres connect function

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
 
 app.get('/', function(request, response) {
+	pg.connect();
   response.send('Hello World!');
 });
 
