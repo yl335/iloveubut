12ec6aa9b3e4cdfe95e756b7f21d363fbdde3327
Sara Lin <slin@splunk.com>
2012-08-27 02:18:46 -0700

    Deleting bunch of files I don't need; Refactored the DATABASE_URL code

--- web.js
+++ web.js
@@ -6,25 +6,10 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path')
-  , pg = require('pg');
+  , pg = require('pg')
+  , DATABASE_URL = process.env.DATABASE_URL;
 
 var app = express();
-
-//DATABASE
-var DATABASE_URL = process.env.DATABASE_URL || "dbname=swallowlink";
-console.log('process.env.DATABASE_URL: %s', process.env.DATABASE_URL);
-console.log('DATABASE_URL: %s', DATABASE_URL);
-pg.connect(DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM weather');
-
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-  
-  query.on('end', function() {
-    client.end();
-  });
-});
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -44,6 +29,25 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+//DATABASE
+if (!DATABASE_URL) {
+  DATABASE_URL = "dbname=swallowlink";
+}
+console.log('process.env.DATABASE_URL: %s', process.env.DATABASE_URL);
+console.log('DATABASE_URL: %s', DATABASE_URL);
+pg.connect(DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM weather');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+  
+  query.on('end', function() {
+    client.end();
+  });
+});
+
+//ROUTING
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
