d38b1ae04058bb1b8c385e7a54ebe73af86f2f39
Sara Lin <slin@splunk.com>
2012-09-01 01:23:07 -0700

    Added database connections

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,49 @@
 /*
  * GET home page.
  */
+ var pg = require('pg')
+   , DATABASE_URL = process.env.DATABASE_URL || "postgres://swallowlink@localhost/iloveubut"
+   , SITE_NAME = 'I Love U But';
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Test' });
+exports.index = function(req, res) {
+	res.render('index', { title: SITE_NAME });
 };
+
+exports.post = function(req, res) {
+	var post = req.body.reason;
+
+	pg.connect(DATABASE_URL, function(err, client) {
+		//INSERT POST
+		var query = client.query('INSERT INTO but VALUES ($1)', [post]);
+
+		//VERIFY POST
+		var query = client.query('SELECT * FROM but');
+		query.on('row', function(row) {
+			console.log(JSON.stringify(row));
+		});
+
+		query.on('end', function() {
+			client.end();
+		});
+	});
+
+	res.send(200);
+}
+
+exports.posts = function(req, res) {
+	console.log("RANDOM POSTS");
+	pg.connect(DATABASE_URL, function(err, client) {
+		//INSERT POST
+		var query = client.query('SELECT * FROM but OFFSET RANDOM() * (SELECT count(*) FROM but) LIMIT 1');
+
+		//VERIFY POST
+		query.on('row', function(row) {
+			console.log(JSON.stringify(row));
+			res.json(200, row.reason);
+		});
+	});
+}
+
+exports.about = function(req, res) {
+	res.render('about', {title: SITE_NAME})
+}
--- web.js
+++ web.js
@@ -5,9 +5,7 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path')
-  , pg = require('pg')
-  , DATABASE_URL = process.env.DATABASE_URL;
+  , path = require('path');
 
 var app = express();
 
@@ -29,27 +27,11 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//DATABASE
-if (!DATABASE_URL) {
-  DATABASE_URL = "postgres://swallowlink@localhost/swallowlink";
-}
-console.log('process.env.DATABASE_URL: %s', process.env.DATABASE_URL);
-console.log('DATABASE_URL: %s', DATABASE_URL);
-pg.connect(DATABASE_URL, function(err, client) {
-  console.log("CLIENT: %s", client);
-  var query = client.query('SELECT * FROM weather');
-
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-  
-  query.on('end', function() {
-    client.end();
-  });
-});
-
 //ROUTING
 app.get('/', routes.index);
+app.post('/post', routes.post);
+app.get('/posts', routes.posts);
+app.get('/about', routes.about);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
